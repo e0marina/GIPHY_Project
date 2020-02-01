4eedb8777b49977eb26aa69b70ba218e28891392
Emily M <30880308+e0marina@users.noreply.github.com>
2020-01-31 18:20:16 -0800

    getting functions in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 // GLOBAL VARIABLES
 //==============================================================
-var topics = [
+// Initial Array of artists
+
+var artists = [
   "picasso",
   "frida kahlo",
   "basquiat",
@@ -10,34 +12,39 @@ var topics = [
   "monet",
   "yayoi kusama"
 ];
-function displayTopicsGifs() {
-  // Here we are building the URL we need to query the database
+function displayArtistGifs() {
+  var artist = $(this).attr("data-name");
+  // Here we are building the URL we need to query GIPHY's database
   var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=picasso&api_key=AO46ZvF2PWKxb55EA1XxuXtwjegfnCH8&limit=10";
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    artist +
+    "&api_key=AO46ZvF2PWKxb55EA1XxuXtwjegfnCH8&limit=10";
 
   // Here we run our AJAX call to the GIPHY API
   $.ajax({
     url: queryURL,
     method: "GET"
-  })
-
-    // we store all of the retreived data inside an object called "response"
-
+  }) // we store all of the retreived data inside an object called "response"
     .then(function(response) {
       //log the query URL
       console.log(queryURL);
       //log the resulting object
       console.log(response);
-      //creates a div to hold the gif
-      $("<div>").append("#gif-area");
-      //retreives the static image
+      //creates a div to hold the gifs
+      var gifDiv = $("<div class ='gif'>");
+      //stores the static image
 
       //displays the static image
-      //retrieves the rating data
+
+      //stores the rating data
       var gifRating = response.data.rating;
       //displays the rating
 
+      //stores the moving gif
+
       //?on click function to make the image a gif
+      //puts the gifs on the page
+      $("#gif-area").append(gifDiv);
     });
 }
 //function for creating buttons with artists
